983d931ecbaed5ce169d836a4a1c41c2363f0244
ilcors-dev <corsetti.luca28@gmail.com>
2023-02-22 15:33:05 +0100

    chore (prototype): first commit with expo+reactnative+tailwindcss(nativewind)

---
+++ prototype/constants/Colors.ts
@@ -0,0 +1,21 @@
+const tintColorLight = '#2f95dc';
+const tintColorDark = '#fff';
+
+export default {
+	light: {
+		text: '#000',
+		background: '#FFDE5A',
+		blue: '#5371FF',
+		tint: tintColorLight,
+		tabIconDefault: '#ccc',
+		tabIconSelected: tintColorLight,
+	},
+	dark: {
+		text: '#000',
+		background: '#FFDE5A',
+		blue: '#5371FF',
+		tint: tintColorDark,
+		tabIconDefault: '#ccc',
+		tabIconSelected: tintColorDark,
+	},
+};
---
+++ prototype/screens/GestioneSpazioScreen.tsx
@@ -0,0 +1,39 @@
+import { StyleSheet } from 'react-native';
+
+import EditScreenInfo from '../components/EditScreenInfo';
+import { Text, View } from '../components/Themed';
+import { RootTabScreenProps } from '../types';
+
+export default function GestioneSpazioScreen({
+	navigation,
+}: RootTabScreenProps<'GestioneSpazio'>) {
+	return (
+		<View style={styles.container}>
+			<Text style={styles.title}>Tab One</Text>
+			<View
+				style={styles.separator}
+				lightColor="#eee"
+				darkColor="rgba(255,255,255,0.1)"
+			/>
+			<p className="font-bold">miao</p>
+			<EditScreenInfo path="/screens/GestioneSpazioScreen.tsx" />
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	title: {
+		fontSize: 20,
+		fontWeight: 'bold',
+	},
+	separator: {
+		marginVertical: 30,
+		height: 1,
+		width: '80%',
+	},
+});
