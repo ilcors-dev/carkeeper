cd0c6d08e1af8a89662d0f480f074598cc65da51
BabelDev0 <leonardo.straccali@gmail.com>
2023-02-25 02:58:31 +0100

    feat (front-end): Changed the graphic appearance of the vehicle list component

--- prototype/components/VehicleSpace/VehicleSpaceItem.tsx
+++ prototype/components/VehicleSpace/VehicleSpaceItem.tsx
@@ -1,15 +1,46 @@
-import { Text, View } from 'react-native';
+import { FontAwesome } from "@expo/vector-icons";
+import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
 
 interface Props {
-	id: string;
+	uuid: string;
 	createdAt: Date;
 }
 
-export const VehicleSpaceItem = ({ id, createdAt }: Props) => {
+export const VehicleSpaceItem = ({ uuid, createdAt }: Props) => {
+	const styles = StyleSheet.create({
+		list: {
+			padding: 20,
+		},
+		carListItem: {
+			backgroundColor: "#5371FF",
+			borderRadius: 20,
+			padding: 20,
+			marginBottom: 20,
+			flexDirection: "row",
+			alignItems: "center",
+		},
+		carListItemText: {
+			color: "#fff",
+			fontSize: 24,
+			marginLeft: 20,
+		},
+		carListItemTextDate: {
+			color: "#fff",
+			fontSize: 18,
+			marginLeft: 20,
+		},
+	});
 	return (
-		<View>
-			<Text>{id}</Text>
-			<Text>{createdAt.toISOString()}</Text>
-		</View>
+		<TouchableOpacity>
+			<View style={styles.carListItem}>
+				<FontAwesome name="car" size={60} color="#fff" />
+				<View>
+					<Text style={styles.carListItemText}>{uuid}</Text>
+					<Text style={styles.carListItemTextDate}>
+						{createdAt.toDateString()}
+					</Text>
+				</View>
+			</View>
+		</TouchableOpacity>
 	);
 };
