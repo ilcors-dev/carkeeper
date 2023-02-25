aef01548813689dfc7d2979cd06788c88937b729
BabelDev0 <leonardo.straccali@gmail.com>
2023-02-25 03:00:18 +0100

    feat (navigation): added navigation to the stack

--- prototype/navigation/index.tsx
+++ prototype/navigation/index.tsx
@@ -3,32 +3,33 @@
  * https://reactnavigation.org/docs/getting-started
  *
  */
-import { FontAwesome } from '@expo/vector-icons';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { FontAwesome } from "@expo/vector-icons";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import {
 	NavigationContainer,
 	DefaultTheme,
 	DarkTheme,
-} from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import * as React from 'react';
-import { ColorSchemeName, Pressable } from 'react-native';
+} from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import * as React from "react";
+import { ColorSchemeName, Pressable } from "react-native";
 
-import Colors from '../constants/Colors';
-import useColorScheme from '../hooks/useColorScheme';
-import ModalScreen from '../screens/ModalScreen';
-import NotFoundScreen from '../screens/NotFoundScreen';
-import VehicleSpaceScreen from '../screens/VehicleSpaceScreen';
-import UserProfileScreen from '../screens/UserProfileScreen';
+import Colors from "../constants/Colors";
+import useColorScheme from "../hooks/useColorScheme";
+import ModalScreen from "../screens/ModalScreen";
+import NotFoundScreen from "../screens/NotFoundScreen";
+import VehicleSpaceScreen from "../screens/VehicleSpaceScreen";
+import UserProfileScreen from "../screens/UserProfileScreen";
 import {
 	RootStackParamList,
 	RootTabParamList,
 	RootTabScreenProps,
-} from '../types';
-import LinkingConfiguration from './LinkingConfiguration';
-import ContextCreation from '../models/ContextCreation';
-import { User } from '../models/User';
-import AuthenticationScreen from '../screens/AuthenticationScreen';
+} from "../types";
+import LinkingConfiguration from "./LinkingConfiguration";
+import ContextCreation from "../models/ContextCreation";
+import { User } from "../models/User";
+import AuthenticationScreen from "../screens/AuthenticationScreen";
+import VehicleAssociationScreen from "../screens/VehicleAssociationScreen";
 
 export default function Navigation({
 	colorScheme,
@@ -38,7 +39,7 @@ export default function Navigation({
 	return (
 		<NavigationContainer
 			linking={LinkingConfiguration}
-			theme={colorScheme === 'dark' ? DarkTheme : DefaultTheme}
+			theme={colorScheme === "dark" ? DarkTheme : DefaultTheme}
 		>
 			<RootNavigator />
 		</NavigationContainer>
@@ -58,7 +59,7 @@ function RootNavigator() {
 
 	const mock = React.useCallback(async () => {
 		try {
-			realm.write(() => realm.create(User, User.generate('123456')));
+			realm.write(() => realm.create(User, User.generate("123456")));
 		} catch (error) {
 			console.log(error);
 		}
@@ -83,11 +84,16 @@ function RootNavigator() {
 				options={{ headerShown: false }}
 			/>
 			<Stack.Screen
+				name="VehicleAssociation"
+				component={VehicleAssociationScreen}
+				options={{ title: "Vehicle Association" }}
+			/>
+			<Stack.Screen
 				name="NotFound"
 				component={NotFoundScreen}
-				options={{ title: 'Oops!' }}
+				options={{ title: "Oops!" }}
 			/>
-			<Stack.Group screenOptions={{ presentation: 'modal' }}>
+			<Stack.Group screenOptions={{ presentation: "modal" }}>
 				<Stack.Screen name="Modal" component={ModalScreen} />
 			</Stack.Group>
 		</Stack.Navigator>
@@ -115,8 +121,8 @@ function BottomTabNavigator() {
 				component={VehicleSpaceScreen}
 				options={({
 					navigation,
-				}: RootTabScreenProps<'HandleVehicleSpace'>) => ({
-					title: 'HandleVehicleSpace',
+				}: RootTabScreenProps<"HandleVehicleSpace">) => ({
+					title: "HandleVehicleSpace",
 					tabBarShowLabel: false,
 					tabBarIcon: ({ color }) => <TabBarIcon name="car" color={color} />,
 					headerShown: false,
@@ -126,7 +132,7 @@ function BottomTabNavigator() {
 				name="HandleUserProfile"
 				component={UserProfileScreen}
 				options={{
-					title: 'Profilo',
+					title: "Profilo",
 					tabBarShowLabel: false,
 					tabBarIcon: ({ color }) => <TabBarIcon name="user" color={color} />,
 					headerShown: false,
@@ -140,7 +146,7 @@ function BottomTabNavigator() {
  * You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
  */
 function TabBarIcon(props: {
-	name: React.ComponentProps<typeof FontAwesome>['name'];
+	name: React.ComponentProps<typeof FontAwesome>["name"];
 	color: string;
 }) {
 	return <FontAwesome size={30} style={{ marginBottom: -3 }} {...props} />;
