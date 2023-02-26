80a1bdda54b5d0c26aa03c10d1b4432fe373d966
ilcors-dev <corsetti.luca28@gmail.com>
2023-02-26 17:59:12 +0100

    feat: vehicle screen layout

--- prototype/screens/VehicleScreen.tsx
+++ prototype/screens/VehicleScreen.tsx
@@ -1,4 +1,4 @@
-import { FontAwesome } from '@expo/vector-icons';
+import { FontAwesome5 } from '@expo/vector-icons';
 import React, { useCallback } from 'react';
 import { Text, TouchableOpacity, View } from 'react-native';
 import { Results } from 'realm';
@@ -26,13 +26,44 @@ export default function VehicleScreen({
 	return (
 		<View className="flex h-screen flex-col p-5 bg-primary">
 			<Logo />
-			<Text>{vehicle.getName()}</Text>
+			<View className="flex flex-col space-y-4">
+				<View className="p-4 px-6 flex flex-row items-center space-x-4 border rounded-lg">
+					<FontAwesome5 name="car" size={40} color="#000" />
+					<Text className="mt-2 text-base font-bold">{vehicle.getName()}</Text>
+				</View>
+				<View className="p-4 px-6 bg-secondary rounded-lg">
+					<View className="flex flex-row justify-around">
+						<View className="flex flex-col space-y-2 items-center">
+							<FontAwesome5 name="gas-pump" size={32} color="#fff" />
+							<Text className="text-sm font-bold text-white">26/60l</Text>
+						</View>
+						<View className="flex flex-col space-y-2 items-center">
+							<FontAwesome5 name="check" size={32} color="#fff" />
+							<Text className="text-sm font-bold text-white">All good</Text>
+						</View>
+						<View className="flex flex-col space-y-2 items-center">
+							<FontAwesome5 name="road" size={32} color="#fff" />
+							<Text className="text-sm font-bold text-white">32576 km</Text>
+						</View>
+					</View>
+				</View>
+				<View className="p-4 px-6 bg-secondary rounded-lg">
+					<View className="flex flex-row justify-around">
+						<Text className="text-white">scadenze</Text>
+					</View>
+				</View>
+				<View className="p-4 px-6 bg-white rounded-lg">
+					<View className="flex flex-row justify-around">
+						<Text className="text-white">scadenze</Text>
+					</View>
+				</View>
+			</View>
 			<TouchableOpacity
-				className="p-2 px-4 rounded-lg bg-secondary flex flex-row space-x-2 justify-center w-24 mx-auto mt-auto mb-20"
+				className="p-2 px-4 rounded-lg bg-secondary flex flex-row space-x-2 justify-center w-28 mx-auto mt-auto mb-20"
 				onPress={remove}
 			>
-				<FontAwesome name="trash" size={16} />
-				<Text>Remove</Text>
+				<FontAwesome5 name="trash" size={16} color="#fff" />
+				<Text className="text-white">Remove</Text>
 			</TouchableOpacity>
 		</View>
 	);
