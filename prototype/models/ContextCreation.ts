8a5c1ef5576b56e29524358a90a8c969e36dafd9
BabelDev0 <leonardo.straccali@gmail.com>
2023-02-27 02:33:03 +0100

    feat (context creation): modified schema and schemaVersion

--- prototype/models/ContextCreation.ts
+++ prototype/models/ContextCreation.ts
@@ -1,11 +1,13 @@
 import { createRealmContext } from '@realm/react';
+import { StatsTravel } from './StatsTravel';
 import { User } from './User';
 import { Vehicle } from './Vehicle';
 import { VehicleDataPiece } from './VehicleDataPiece';
+import { ChatMessage } from './ChatMessage';
 
 const config = {
-	schema: [Vehicle, User, VehicleDataPiece],
-	schemaVersion: 8,
+	schema: [Vehicle, User, VehicleDataPiece, StatsTravel, ChatMessage],
+	schemaVersion: 19,
 };
 
 export default createRealmContext(config);
